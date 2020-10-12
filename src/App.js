ec7ce70cba07956ce2ca4aa346ef697fb57be989
Ram kumar <rk16013093@gmail.com>
2020-10-13 00:40:07 +0530

    route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter ,Route  } from 'react-router-dom'
+import Single from './components/SingleStudent';
 //import Student from './components/Student';
 
 function App() {
   return (
     <div>
-     <Home />
+    <BrowserRouter >
+       {/* <Home /> */}
+      <Route  exact path='/true-exam' component={Home} />
+      <Route  path='/student/:id'  component={Single}/>
+    </BrowserRouter>
+    
     
     </div>
   );
