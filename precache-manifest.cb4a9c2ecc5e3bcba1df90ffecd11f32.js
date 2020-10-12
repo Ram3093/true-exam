c23cfbc376bbd372e0e5232ab1353508f2af18e1
Ram kumar <rk16013093@gmail.com>
2020-10-12 18:06:22 +0530

    Updates

---
+++ precache-manifest.cb4a9c2ecc5e3bcba1df90ffecd11f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddee84aad414ec4a3a7fe9aada255df",
+    "url": "/true-exam/index.html"
+  },
+  {
+    "revision": "47a3bb0afe6d6b5217d1",
+    "url": "/true-exam/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "38be3d7a9777d3b3d9d5",
+    "url": "/true-exam/static/css/main.92821e80.chunk.css"
+  },
+  {
+    "revision": "47a3bb0afe6d6b5217d1",
+    "url": "/true-exam/static/js/2.25832f81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/true-exam/static/js/2.25832f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38be3d7a9777d3b3d9d5",
+    "url": "/true-exam/static/js/main.e86ba934.chunk.js"
+  },
+  {
+    "revision": "4046475a1fa1fd10774b",
+    "url": "/true-exam/static/js/runtime-main.f916afee.js"
+  }
+]);
